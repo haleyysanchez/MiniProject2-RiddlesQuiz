ef0c5c7970b24707607f39eca708728e7911e46e
haleyysanchez <haleymsanchez910@gmail.com>
2020-06-04 14:13:42 -0400

    Initial Commit

---
+++ MiniProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by Haley Sanchez on 6/4/20.
+//  Copyright © 2020 Haley Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
